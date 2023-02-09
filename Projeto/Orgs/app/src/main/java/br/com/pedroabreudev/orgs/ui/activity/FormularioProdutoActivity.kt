e8452ec40e5a5ed946717a1290e14897c6861a14
Pedro Henrique Abreu <pedroabreu.dev@gmail.com>
2023-02-09 16:29:33 -0400

    feat: Modificando as Dependências.

---
+++ Projeto/Orgs/app/src/main/java/br/com/pedroabreudev/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,13 @@
+package br.com.pedroabreudev.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.pedroabreudev.orgs.R
+
+class FormularioProdutoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_produto)
+    }
+}
