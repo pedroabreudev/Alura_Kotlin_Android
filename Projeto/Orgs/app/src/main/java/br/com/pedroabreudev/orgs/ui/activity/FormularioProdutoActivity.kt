ce1ac3f2f6df025f9ad0f68c57ad2e07184a7908
Pedro Henrique Abreu <pedroabreu.dev@gmail.com>
2023-02-09 16:53:46 -0400

    feat: Criando o Produto.

--- Projeto/Orgs/app/src/main/java/br/com/pedroabreudev/orgs/ui/activity/FormularioProdutoActivity.kt
+++ Projeto/Orgs/app/src/main/java/br/com/pedroabreudev/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -2,12 +2,39 @@ package br.com.pedroabreudev.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import br.com.pedroabreudev.orgs.R
+import br.com.pedroabreudev.orgs.model.Produto
+import java.math.BigDecimal
 
 class FormularioProdutoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_formulario_produto)
+
+        val botaoSalvar = findViewById<Button>(R.id.botao_salvar)
+        botaoSalvar.setOnClickListener {
+            val campoNome = findViewById<EditText>(R.id.nome)
+            val nome = campoNome.text.toString()
+            val campoDescricao = findViewById<EditText>(R.id.descricao)
+            val descricao = campoDescricao.text.toString()
+            val campoValor = findViewById<EditText>(R.id.valor)
+            val valor = campoValor.text.toString()
+            val valorProduto = if (valor.isBlank()) {
+                BigDecimal.ZERO
+            } else {
+                BigDecimal(valor)
+            }
+            
+            val produtoCriado = Produto(
+                nome = nome,
+                descricao = descricao,
+                valor = valorProduto
+            )
+            Log.i("Formulario Produto Activity", "onCreate: $produtoCriado")
+        }
     }
 }
