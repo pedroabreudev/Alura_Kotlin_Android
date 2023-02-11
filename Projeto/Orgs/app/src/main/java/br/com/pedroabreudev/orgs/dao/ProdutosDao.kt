e89d7fa80e335a9ebb0dd2a9e80faf069c40f07e
Pedro Henrique Abreu <pedroabreu.dev@gmail.com>
2023-02-11 12:29:29 -0400

    init: Curso de Personalização do App.

--- Projeto/Orgs/app/src/main/java/br/com/pedroabreudev/orgs/dao/ProdutosDao.kt
+++ Projeto/Orgs/app/src/main/java/br/com/pedroabreudev/orgs/dao/ProdutosDao.kt
@@ -1,6 +1,7 @@
 package br.com.pedroabreudev.orgs.dao
 
 import br.com.pedroabreudev.orgs.model.Produto
+import java.math.BigDecimal
 
 class ProdutosDao {
 
@@ -13,6 +14,12 @@ class ProdutosDao {
     }
 
     companion object {
-        private val produtos = mutableListOf<Produto>()
+        private val produtos = mutableListOf<Produto>(
+            Produto(
+                nome = "Sala de frutas",
+                descricao = "Laranja, maçãs e uva",
+                valor = BigDecimal("19.83")
+            )
+        )
     }
 }
