9f64ad2483b7a254cbef602a4c0ce7a04f3227ed
Pedro Henrique Abreu <pedroabreu.dev@gmail.com>
2023-08-07 20:26:40 -0400

    feat: Menus nas telas de Detalhes

--- Projeto/Orgs/app/src/main/java/br/com/pedroabreudev/orgs/ui/activity/DetalhesProdutoActivity.kt
+++ Projeto/Orgs/app/src/main/java/br/com/pedroabreudev/orgs/ui/activity/DetalhesProdutoActivity.kt
@@ -2,11 +2,16 @@ package br.com.pedroabreudev.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import br.com.pedroabreudev.orgs.R
 import br.com.pedroabreudev.orgs.databinding.ActivityDetalhesProdutoBinding
 import br.com.pedroabreudev.orgs.extensions.formataParaMoedaBrasileira
 import br.com.pedroabreudev.orgs.extensions.tentaCarregarImagem
 import br.com.pedroabreudev.orgs.model.Produto
+
+private const val TAG = "DetalhesProduto"
 
 class DetalhesProdutoActivity : AppCompatActivity() {
 
@@ -18,6 +23,24 @@ class DetalhesProdutoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         tentaCarregarProduto()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_detalhes_produto, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_detalhes_produto_remover -> {
+                Log.i(TAG, "onOptionsItemSelected: remover")
+            }
+
+            R.id.menu_detalhes_produto_editar -> {
+                Log.i(TAG, "onOptionsItemSelected: editar")
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun tentaCarregarProduto() {
